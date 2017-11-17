53d5584859e271fa0e90eec1d28234bdd503596c
d86leader <d86leader@github.com>
2017-11-17 17:13:13 +0700

    added and updated stubs

--- inputs/Input.h
+++ inputs/Input.h
@@ -1,7 +1,15 @@
+#pragma once
+
+#include <memory>
+
+#include "inputs/Command.h"
+#include "objects/AnimateObject.h"
 #include "objects/GameField.h"
+
+using std::shared_ptr;
 
 class Input
 {
 	public:
-		virtual void update(GameField) = 0;
+		virtual Command plan(GameField, shared_ptr<AnimateObject>) = 0;
 };

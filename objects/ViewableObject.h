07c4c9e9f15ed7a87044906fab572402a9118a8e
d86leader <d86leader@mail.com>
2017-12-08 20:49:22 +0700

    made ViewableObject use color wrapper instead of short

--- objects/ViewableObject.h
+++ objects/ViewableObject.h
@@ -3,6 +3,7 @@
 #include <ncurses.h>
 
 #include "objects/MapConstants.h"
+#include "view/Color.h"
 
 //Interface to print items on screen. Will be extended in the future probably
 
@@ -12,8 +13,8 @@ public:
 	virtual char get_form() = 0;
 
 	//fuck fuck fuck old-c-style types
-	virtual short get_bg_color()   = 0;
-	virtual short get_body_color() = 0;
+	virtual Color get_bg_color()   = 0;
+	virtual Color get_body_color() = 0;
 
 	virtual Coordinate get_x(void) = 0;
 	virtual Coordinate get_y(void) = 0;

f49b5610c81fd7b6a3eb706179bd772ab08ee701
tatsar <32083374+tatsar@users.noreply.github.com>
2017-12-13 14:04:03 +0700

    изменили for, убдали поля длины и высоты
    for из старого на новый11

--- objects/Map.cpp
+++ objects/Map.cpp
@@ -2,15 +2,17 @@
 
 Map::Map(Coordinate height,Coordinate width)
 {
-	m_height = height;
-	m_width = width;
+//	m_height = height;
+//	m_width = width;
 	m_map.resize(width);
-	for(Coordinate i = 0; i < width; i++)
-		m_map[i].resize(height);
+//	for(Coordinate i = 0; i < width; i++)
+	for( auto i : m_map)
+		i->resize(height);
 }
 Map & Map::change_block(Coordinate height,Coordinate width, Block block)
 {
-	if(height >= 0 && height < m_height && width >= 0 && width < m_width)
+//	if(height >= 0 && height < m_height && width >= 0 && width < m_width)
+	if(height < m_map[i].size && wight < m_map.size)
 		m_map.at(width).at(height) = block;
 	return *this;
 }

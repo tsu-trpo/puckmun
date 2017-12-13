46405a75346f497f8f3c7024f50c203956f5bb4c
tatsar <32083374+tatsar@users.noreply.github.com>
2017-12-13 14:16:44 +0700

    - поля, +возврат длины и ширины

--- objects/Map.h
+++ objects/Map.h
@@ -9,11 +9,11 @@ using namespace std;
 
 class Map
 {
-	Coordinate m_height;
-	Coordinate m_width;
 	vector<vector<Block> > m_map;
 		
 public:
 	Map(Coordinate,Coordinate);
 	Map & change_block(Coordinate,Coordinate, Block);
+	Coordinate get_height();
+	Coordinate get_width();
 };

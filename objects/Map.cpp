2269c8776691db6869f32da77718ae7086003086
alex8h <tat7446@yandex.ru>
2017-12-13 14:18:33 +0700

    Добавлены функции возврата длины и ширины

--- objects/Map.cpp
+++ objects/Map.cpp
@@ -2,17 +2,26 @@
 
 Map::Map(Coordinate height,Coordinate width)
 {
-//	m_height = height;
-//	m_width = width;
+
 	m_map.resize(width);
-//	for(Coordinate i = 0; i < width; i++)
 	for( auto i : m_map)
-		i->resize(height);
+		i.resize(height);
 }
 Map & Map::change_block(Coordinate height,Coordinate width, Block block)
 {
-//	if(height >= 0 && height < m_height && width >= 0 && width < m_width)
-	if(height < m_map[i].size && wight < m_map.size)
+	if(height < m_map[0].size() && width < m_map.size())
 		m_map.at(width).at(height) = block;
 	return *this;
 }
+
+Coordinate Map::get_width()
+{
+	return m_map.size();
+}
+
+Coordinate Map::get_height()
+{
+	return m_map[0].size();
+}
+
+

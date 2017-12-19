1df7ad17cc3b7a7050d3984c5cf325f666c4ecbb
tatsar <32083374+tatsar@users.noreply.github.com>
2017-12-19 09:51:43 +0700

    убраны лишние include

--- objects/TheMan.h
+++ objects/TheMan.h
@@ -1,13 +1,7 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
-#include "objects/Block.h"
-#include "control/MoveDirection.h"
-#include "objects/MapConstants.h"
-#include "control/MoveConstants.h"
 #include "objects/AnimateObject.h"
-using namespace std;
 
 class TheMan: public AnimateObject
 {	

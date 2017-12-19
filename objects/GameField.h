a032996c7be82a5ca19e0fa9731aed2d79b949d5
d86leader <d86leader@mail.com>
2017-12-19 17:25:03 +0700

    added list of objects and accessor to gameField

--- objects/GameField.h
+++ objects/GameField.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include <vector>
+#include <memory>
+
+#include "AnimateObject.h"
+
+using std::shared_ptr;
+
 class GameField
 {
-	
+	std::vector< shared_ptr<AnimateObject> > m_objects;
+
+public:
+	const std::vector< shared_ptr<AnimateObject> >& get_objects();
 };

0fb6b3e03762bd71e050a6b0bb4e506cefab13aa
alex8h <32083278+alex8h@users.noreply.github.com>
2017-12-19 09:40:04 +0700

    внесла изменения в соответствии с комментариями

--- objects/Map.cpp
+++ objects/Map.cpp
@@ -4,23 +4,26 @@ Map::Map(Coordinate height,Coordinate width)
 {
 
 	m_map.resize(width);
-	for( auto i : m_map)
-		i.resize(height);
+	for( auto &line : m_map)
+		line->resize(height);
 }
 Map & Map::change_block(Coordinate height,Coordinate width, Block block)
 {
-	if(height < m_map[0].size() && width < m_map.size())
+	if(height < get_width() && width < get_height())
 		m_map.at(width).at(height) = block;
 	return *this;
 }
 
 Coordinate Map::get_width()
-{
+{	
 	return m_map.size();
 }
 
 Coordinate Map::get_height()
 {
+	
+	if (get_width() == 0) 
+    		return 0;
 	return m_map[0].size();
 }
 

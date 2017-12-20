ab41b904d3476142280487d6335f294c197339b5
d86leader <d86leader@mail.com>
2017-12-20 17:28:41 +0700

    added event class

---
+++ control/Event.cpp
@@ -0,0 +1,67 @@
+#include "control/Event.h"
+#include "control/MoveConstants.h"
+
+Event::Event(const Event::Type&   type,
+             const ObjectPtr&     object,
+             const Coordinate&    coord_x,
+             const Coordinate&    coord_y,
+             const MoveDirection& dir,
+             const Block&         block)
+	: m_type                  (type)
+	, m_object_argument       (object)
+	, m_coordinate_argument_x (coord_x)
+	, m_coordinate_argument_y (coord_y)
+	, m_direction_argument    (dir)
+	, m_block_argument        (block)
+{}
+
+
+Event event_move(MoveDirection dir, ObjectPtr obj)
+{
+	return Event(Event::Type::Move, obj, // non-standart
+	             StandartX, StandartY,   // standart
+	             dir,                    // non-standart
+	             StandartBlock);         // standart
+}
+Event event_kill(ObjectPtr obj)
+{
+	return Event(Event::Type::Kill, obj, // only non-standart
+	             StandartX, StandartY, StandartDirection, StandartBlock);
+}
+Event event_promote(ObjectPtr obj)
+{
+	return Event(Event::Type::Promote, obj, // only non-standart
+	             StandartX, StandartY, StandartDirection, StandartBlock);
+}
+Event event_demote(ObjectPtr obj)
+{
+	return Event(Event::Type::Demote, obj, // only non-standart
+	             StandartX, StandartY, StandartDirection, StandartBlock);
+}
+Event event_eat_point(Coordinate x, Coordinate y)
+{
+	return Event(Event::Type::EatPoint,             // non-standart
+	             ObjectPtr(nullptr),                // standarty
+	             x, y,                              // non-standart
+	             StandartDirection, StandartBlock); // standart
+}
+Event event_destroy_wall(Coordinate x, Coordinate y)
+{
+	return Event(Event::Type::DestroyWall,          // non-standart
+	             ObjectPtr(nullptr),                // standarty
+	             x, y,                              // non-standart
+	             StandartDirection, StandartBlock); // standart
+}
+Event event_add_wall(Coordinate x, Coordinate y, Block block)
+{
+	return Event(Event::Type::AddWall, // non-standart
+	             ObjectPtr(nullptr),   // standarty
+	             x, y,                 // non-standart
+	             StandartDirection,    // standart
+	             block);               // non-standart
+}
+Event event_nothing()
+{
+	return Event(Event::Type::Nothing, ObjectPtr(nullptr),
+	             StandartX, StandartY, StandartDirection, StandartBlock);
+}

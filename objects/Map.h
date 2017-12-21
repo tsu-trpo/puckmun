57e5d7527b9ceb7ac490f24dcd2fb379fbeae2e4
alex8h <32083278+alex8h@users.noreply.github.com>
2017-12-21 18:05:04 +0700

    Update Map.h

--- objects/Map.h
+++ objects/Map.h
@@ -12,8 +12,8 @@ class Map
 	vector<vector<Block> > m_map;
 		
 public:
-	Map(Coordinate,Coordinate);
-	Map & change_block(Coordinate,Coordinate, Block);
+	Map(Coordinate, Coordinate);
+	Map & change_block(Coordinate, Coordinate, Block);
 	Coordinate get_height();
 	Coordinate get_width();
 };

dcb6818b93aafea2f8558bd889cc79e34690dede
d86leader <d86leader@mail.com>
2017-12-22 15:52:10 +0700

    added number of points left to gameField

--- objects/GameField.h
+++ objects/GameField.h
@@ -5,6 +5,7 @@
 
 #include "GameObject.h"
 #include "objects/Map.h"
+#include "objects/MapConstants.h"
 
 using std::shared_ptr;
 
@@ -12,4 +13,5 @@ struct GameField
 {
 	std::vector< shared_ptr<GameObject> > objects;
 	Map map;
+	PointsNumber points_left;
 };

0be37c72cd25440c471329b3baefb8f5e55774e7
d86leader <d86leader@mail.com>
2017-12-23 22:41:46 +0700

    wrote simple test to show map

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <ncurses.h>
+#include <typeinfo>
 
 #include "objects/MapLoader.h"
+#include "objects/GameField.h"
+#include "view/GameRender.h"
 
 int main()
 {
 	Map map = basic_load_map("../maps/test_map.bpm");
 	print_map(map);
+
+	GameField field {{}, map, 0};
+	GameRender render;
+
+	render.redraw_complete(field);
+	getch();
 
 	return 0;
 }

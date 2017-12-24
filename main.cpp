7658d3faee4bf5d0d458f0011464c8ed5ad6a774
d86leader <d86leader@mail.com>
2017-12-25 00:41:29 +0700

    example now uses efficient redraw methods

--- main.cpp
+++ main.cpp
@@ -34,30 +34,30 @@ int main()
 			case 'k':
 			case 'w':
 			case KEY_UP:
+				render.redraw_object_pre_move(field, *obj, x, y-1);
 				y -= 1;
 				obj->set_y(y);
-				render.redraw_complete(field);
 				break;
 			case 'j':
 			case 's':
 			case KEY_DOWN:
 				y += 1;
 				obj->set_y(y);
-				render.redraw_complete(field);
+				render.redraw_object_post_move(field, x, y-1, *obj);
 				break;
 			case 'h':
 			case 'a':
 			case KEY_LEFT:
 				x -= 1;
 				obj->set_x(x);
-				render.redraw_complete(field);
+				render.redraw_object_post_move(field, x+1, y, *obj);
 				break;
 			case 'l':
 			case 'd':
 			case KEY_RIGHT:
 				x += 1;
 				obj->set_x(x);
-				render.redraw_complete(field);
+				render.redraw_object_post_move(field, x-1, y, *obj);
 				break;
 			default:
 				exit = true;

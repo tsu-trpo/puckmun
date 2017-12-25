eb6f083f05dac9479271d123d558c4ae29140782
d86leader <d86leader@mail.com>
2017-12-26 00:14:56 +0700

    deleted unused constants again

--- objects/MapConstants.h
+++ objects/MapConstants.h
@@ -12,11 +12,4 @@ typedef size_t PointsNumber;
 // Тип, хранящий координаты
 typedef size_t Coordinate;
 
-// Значения стандартных координат для разных вещей
-Coordinate StandartX = 0;
-Coordinate StandartY = 0;
-
-// Значение стандартного блока
-Block StandartBlock = Block::Space;
-
 // vim: tw=78

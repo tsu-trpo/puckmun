5c736720ff7b10e6b66fc2655cb2d58d09988659
tatsar <tatsar31@rambler.ru>
2017-12-27 22:33:34 +0700

    определены функци из ViewableObject.h

--- objects/Ghost.cpp
+++ objects/Ghost.cpp
@@ -13,3 +13,18 @@ Ghost & Ghost::set_current(MoveDirection next)
 	m_current = next;
 	return *this;
 }
+
+char Ghost::get_form() const
+{
+	return 'M';
+}
+
+Color Ghost::get_bg_color() const
+{
+	return Color::Black;
+}
+
+Color Ghost::get_body_color() const
+{
+	return Color::Cyan;
+}
--- objects/Ghost.h
+++ objects/Ghost.h
@@ -7,4 +7,7 @@ public:
 	Ghost();
 	MoveDirection tick() override;
 	Ghost & set_current(MoveDirection);
+	char get_form() const override;
+	Color get_bg_color() const override;
+	Color get_body_color() const override;
 };

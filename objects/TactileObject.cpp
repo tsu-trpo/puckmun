d8ab60c878b1aa60e63eb0349c83ee75e0915a7d
d86leader <d86leader@mail.com>
2017-12-29 00:17:59 +0700

    wrote tactile object, object that can be touched

---
+++ objects/TactileObject.cpp
@@ -0,0 +1,20 @@
+#include "objects/TactileObject.h"
+
+#include "control/Event.h"
+
+Event TactileObject::touch(shared_ptr<const TactileObject> other) const
+{
+	return other->touch(shared_from_this());
+}
+
+
+Event TactileObject::touch(shared_ptr<const TheMan>) const
+{
+	return Events::make_nothing();
+}
+
+
+Event TactileObject::touch(shared_ptr<const Ghost>) const
+{
+	return Events::make_nothing();
+}
